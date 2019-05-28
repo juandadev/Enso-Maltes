6dbd1cf9fbe1af1e1cd8b692e528942117bfad8c
juan <mtzn_juandaniel@yahoo.com.mx>
2019-05-28 13:09:54 -0600

    Menu desplegable para celulares (landing page)

--- js/landing.js
+++ js/landing.js
@@ -2,3 +2,16 @@ var btnInicio = document.getElementById('visit');
 btnInicio.addEventListener('click', function () {
     window.location = "inicio.html";
 });
+
+//Menú desplegable
+var btnOpen = document.getElementById('open');
+var btnClose = document.getElementById('close');
+var sideNav = document.getElementById('sidenav');
+
+btnOpen.addEventListener('click', function () {
+    sideNav.style.width = "100%";
+});
+
+btnClose.addEventListener('click', function () {
+    sideNav.style.width = "0";
+});
