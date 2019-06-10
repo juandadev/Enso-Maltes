108cfe5634211b107d1abdb26a3b6649c3851b5b
juan <mtzn_juandaniel@yahoo.com.mx>
2019-06-10 00:52:56 -0600

    creando conexion y obteniendo los articulos a mostrar

--- inicio.php
+++ inicio.php
@@ -1,3 +1,19 @@
 <?php
+require 'private/config.php';
+require 'php/functions.php';
+
+$con = connection($db_config);
+
+if (!$con) {
+    header('Location: error.php');
+}
+
+$posts = obtenerPost($blog_config['posts_per_page'], $con);
+$principal = $posts[0];
+
+if (!$posts) {
+    header('Location: error.php');
+}
+
 require 'views/inicio.view.php';
 ?>
