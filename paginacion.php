c6147634e021c2f176b787874c420c99ba8975c9
juan <mtzn_juandaniel@yahoo.com.mx>
2019-06-10 00:53:31 -0600

    Función de la paginación de los articulos

--- paginacion.php
+++ paginacion.php
@@ -1,7 +1,48 @@
+<?php $paginas = noPaginas($blog_config['posts_per_page'], $con); ?>
+
 <section class="paginacion">
     <ul>
-        <li class="disabled"><i class="fas fa-chevron-circle-left"></i></li>
-        <li><a href="#"><div class="circle"></div></a></li>
-        <li class="disabled"><i class="fas fa-chevron-circle-right"></i></li>
+        <?php if(actual() === 1): ?>
+        <li class="disabled">
+            <i class="fas fa-chevron-circle-left"></i>
+        </li>
+        <?php else: ?>
+        <li>
+            <a href="inicio.php?p=<?php echo actual() - 1; ?>">
+                <i class="fas fa-chevron-circle-left"></i>
+            </a>
+        </li>
+        <?php endif; ?>
+
+        <?php for($i = 1; $i <= $paginas; $i++): ?>
+
+            <?php if(actual() === $i): ?>
+            <li>
+                <a href="inicio.php?p=<?php echo $i; ?>">
+                    <div class="circle actual"></div>
+                </a>
+            </li>
+            <?php else: ?>
+            <li>
+                <a href="inicio.php?p=<?php echo $i; ?>">
+                    <div class="circle"></div>
+                </a>
+            </li>
+            <?php endif; ?>
+
+        <?php endfor; ?>
+
+        <?php if(actual() == $paginas): ?>
+        <li class="disabled">
+            <i class="fas fa-chevron-circle-right"></i>
+        </li>
+        <?php else: ?>
+        <li>
+            <a href="inicio.php?p=<?php echo actual() + 1; ?>">
+                <i class="fas fa-chevron-circle-right"></i>
+            </a>
+        </li>
+        <?php endif; ?>
+
     </ul>
 </section>
