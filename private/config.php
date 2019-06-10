90bb8e257efec6136fc8ff91f3a25e3628dd2dfe
juan <mtzn_juandaniel@yahoo.com.mx>
2019-06-09 18:51:45 -0600

    Configuracion de BD y detalles de paginación

---
+++ private/config.php
@@ -0,0 +1,12 @@
+<?php
+$db_config = array(
+    'database' => 'enso_maltes',
+    'user' => 'juan',
+    'pass' => 'juan15410596'
+);
+
+$blog_config = array(
+    'posts_per_page' => '6';
+    'images' => 'img/'
+);
+?>
