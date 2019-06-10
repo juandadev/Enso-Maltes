ad8a09574ee76c52bef373883688d9e836af519e
juan <mtzn_juandaniel@yahoo.com.mx>
2019-06-10 00:58:51 -0600

    Pagina que indica si hubo algun error de cualquier tipo

---
+++ error.php
@@ -0,0 +1,14 @@
+<?php
+require 'private/config.php';
+require 'views/header.php';
+?>
+
+<main>
+    <div class="error">
+        <h2>Ups! algo ha salido mal</h2>
+    </div>
+</main>
+
+<?php
+require 'views/footer.php';
+?>
